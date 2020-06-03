3282c618271f7feb46ce0f4a42ce8d47e0bf7bc3
Evgeny <bespalove.mir@gmail.com>
2020-06-04 00:03:21 +0300

    Добавляет валидацию вводимого адреса

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/a12f8fae04f68edb013f/maintainability)](https://codeclimate.com/github/Evgenymir/frontend-project-lvl3/maintainability)
-[![Node.js CI](https://github.com/Evgenymir/frontend-project-lvl3/workflows/Node.js%20CI/badge.svg)](https://github.com/Evgenymir/frontend-project-lvl3/workflows/Node.js%20CI/badge.svg)
+[![Node.js CI](https://github.com/Evgenymir/frontend-project-lvl3/workflows/Node.js%20CI/badge.svg)](https://github.com/Evgenymir/frontend-project-lvl3/actions)
 
 Ссылка на приложение [RSS feed](https://frontend-project-lvl3-h112y1tj6.now.sh/)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import * as yup from 'yup';
+import { watch } from 'melanke-watchjs';
 
-console.log('test');
+const schema = yup.string().url();
+const isValidateUrl = (url) => schema.isValid(url);
+
+const runProgramm = () => {
+  const form = document.querySelector('.j-form');
+  const input = form.url;
+  const button = form.elements.submit;
+
+  const state = {
+    processState: 'waiting',
+    validationState: 'valid',
+    inputValue: '',
+    errors: [],
+  };
+
+  watch(state, ['processState', 'validationState'], () => {
+    switch (state.processState) {
+      case 'waiting': {
+        input.classList.remove('is-invalid');
+        button.disabled = true;
+        break;
+      }
+      case 'filling': {
+        if (state.validationState === 'valid') {
+          input.classList.remove('is-invalid');
+          input.classList.add('is-valid');
+        } else {
+          input.classList.add('is-invalid');
+          input.classList.remove('is-valid');
+        }
+        button.disabled = state.validationState === 'invalid';
+        break;
+      }
+      default: {
+        throw new Error(`Unknown state ${state.processState}`);
+      }
+    }
+  });
+
+  input.addEventListener('input', (e) => {
+    const valueUrl = e.target.value.trim();
+    if (valueUrl !== '') {
+      state.processState = 'filling';
+      isValidateUrl(valueUrl).then((valid) => {
+        if (valid) {
+          state.validationState = 'valid';
+        } else {
+          state.validationState = 'invalid';
+        }
+      });
+    } else {
+      state.validationState = 'empty';
+      state.processState = 'waiting';
+    }
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log('Send');
+  });
+};
+
+runProgramm();
