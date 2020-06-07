b58f4d58d3241639fadb12ef47c27addd0ea6a04
Evgeny <bespalove.mir@gmail.com>
2020-06-07 19:43:08 +0300

    Добавляет скачивание потока и обработку ошибок

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/main.scss';
 import axios from 'axios';
 import * as yup from 'yup';
 import { watch } from 'melanke-watchjs';
 
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 const schema = yup.string().url();
 const isValidateUrl = (url) => schema.isValid(url);
 
 const runProgramm = () => {
   const form = document.querySelector('.j-form');
   const input = form.url;
+  const errorBlock = form.querySelector('.error-block');
   const button = form.elements.submit;
+  const spinner = button.querySelector('.spinner-block');
 
   const state = {
     processState: 'waiting',
     validationState: 'valid',
     inputValue: '',
-    errors: [],
+    errors: {},
   };
 
   watch(state, ['processState', 'validationState'], () => {
     switch (state.processState) {
       case 'waiting': {
         input.classList.remove('is-invalid');
+        input.classList.remove('is-valid');
+        errorBlock.classList.remove('is-error-active');
         button.disabled = true;
         break;
       }
@@ -29,11 +34,33 @@ const runProgramm = () => {
         if (state.validationState === 'valid') {
           input.classList.remove('is-invalid');
           input.classList.add('is-valid');
+          errorBlock.classList.remove('is-error-active');
+          errorBlock.textContent = state.errors.valid;
         } else {
           input.classList.add('is-invalid');
           input.classList.remove('is-valid');
+          errorBlock.classList.add('is-error-active');
+          errorBlock.textContent = state.errors.invalid;
         }
         button.disabled = state.validationState === 'invalid';
+        break;
+      }
+      case 'sending': {
+        button.disabled = true;
+        spinner.classList.add('is-active-spinner');
+        break;
+      }
+      case 'finished': {
+        input.value = '';
+        input.classList.remove('is-valid');
+        spinner.classList.remove('is-active-spinner');
+        break;
+      }
+      case 'failed': {
+        spinner.classList.remove('is-active-spinner');
+        button.disabled = true;
+        input.classList.add('is-invalid');
+        errorBlock.textContent = state.errors.network;
         break;
       }
       default: {
@@ -49,8 +76,11 @@ const runProgramm = () => {
       isValidateUrl(valueUrl).then((valid) => {
         if (valid) {
           state.validationState = 'valid';
+          state.errors.valid = '';
+          state.inputValue = valueUrl;
         } else {
           state.validationState = 'invalid';
+          state.errors.invalid = 'Is not valid url';
         }
       });
     } else {
@@ -62,7 +92,18 @@ const runProgramm = () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    console.log('Send');
+    state.processState = 'sending';
+    axios.get(`${corsProxy}${state.inputValue}`)
+      .then((response) => {
+        console.log(response);
+        state.processState = 'finished';
+      })
+      .catch((error) => {
+        state.processState = 'failed';
+        state.errors.network = 'Network error, please try again later.';
+        console.log(error);
+        throw error;
+      });
   });
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -14,6 +15,30 @@ module.exports = {
       }, {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins() {
+                return [
+                  autoprefixer,
+                ];
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
       },
     ],
   },
